54c10b3b6a66a4ffc96fb50c0085d085b3aae5b7
Edwin <edwincastaneda0801@gmail.com>
2024-10-17 17:02:36 -0500

    added playlists routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(require("morgan")("dev"));
 app.use(express.json());
 
 app.use(require("./api/auth").router);
+app.use("/playlists", require("./api/playlists"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
